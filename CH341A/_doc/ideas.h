0357489c118971a71042bca6a53be7672dd93b48
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2023-10-26 20:28:56 +0200

    Update docs

--- CH341A/_doc/ideas.h
+++ CH341A/_doc/ideas.h
@@ -6,13 +6,15 @@
 - functionality similar to Bus Pirate
 - Lua scripting? I2C START/STOP/READ/WRITE
 - OLED display test / control
-- generic I2C Smart Battery detect / info
 - BQ8030, BQ8050 modules
-- BME280 module
 - generic IO -> WS2812
 - generic IO -> SHT11
 - PCF8574 test board?
 - DS1307 RTC
+- MDIO
+- MDIO: TDR using some switch chip
+- SPI: nRF24L01
+	- capture logitech keyboard communication
 
 */
 
