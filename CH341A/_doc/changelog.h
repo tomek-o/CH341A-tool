0e0ee98c79a1687ea58e822be1b6de0f85e80ebf
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2023-11-07 20:21:32 +0100

    Fill up changelog

--- CH341A/_doc/changelog.h
+++ CH341A/_doc/changelog.h
@@ -4,5 +4,21 @@
 
 /** \page changelog Changelog
 
+Version 1.0 - first release
+- detecting I2C devices
+- writing/reading raw I2C data
+- reading Smart Battery information
+- writing/reading CH341A GPIO
+- reading data from some popular I2C sensors:
+	- BME280
+	- BMP180
+	- Si7021
+- writing to SSD1306-based OLED display
+- controlling RDA5807M FM radio module
+
+Version 1.1
+- added reading temperature from LM75 I2C sensor
+
+
 
 */
